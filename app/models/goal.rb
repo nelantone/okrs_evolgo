e7ab12589b5e7d4bbc3029d1582d07055295a16f
Tonio Serna <antonelo@gmail.com>
2022-02-22 11:05:37 +0100

    Add model relations/validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,6 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  has_many :key_results, dependent: :destroy
+
+  validates :title, :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :goals, dependent: :destroy
+  has_many :key_results, through: :goals
+
+  validates :owner, presence: true
 end
